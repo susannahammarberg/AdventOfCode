fef2786c6cff9a1d5049df516f3d77758a34abbf
Susanna Hammarberg <sanna.ham4@gmail.com>
2023-10-21 17:28:02 +0200

    Update lucka6.py
    part 2
    took only 15 min very proud

--- 2021/lucka6.py
+++ 2021/lucka6.py
@@ -22,7 +22,6 @@ def read_test():
 
 data = read_input()
 #data = read_test()
-#data = [int(dat) for dat in data] # turn the items to ints
 
 print('starting potint fishes',data)
 # part 1
@@ -30,33 +29,59 @@ print('starting potint fishes',data)
 Each day, a 0 becomes a 6 and adds a new 8 to the end of the list,
 while each other number decreases by 1 if it was present at the start of the day.
 """
+def do_day1():
+    days = 6
+    for day in range(days):
+        add_fishes = 0
+        add_fishes_idx = []
+        
+        for idx, fish in enumerate(data):
+            if fish == 0:
+                add_fishes += 1
+                add_fishes_idx.append(idx)
+    
+    
+        # update fish count
+        data = [fish-1 for fish in data]
+        
+        # reset fishes to 6 and add new fish
+        for idx in add_fishes_idx:
+            data[idx] = 6
+            data.append(8)
+            
+       # print('at the end of the day',data)
+        
+#do_day1()
+#print('\n' + 'result part 1: ', len(data) )
 
-days = 80
+"""
+part 2: 
+Idea: instead of save lifetime of each fish, 
+save the nbr of fish for each lifetime,
+there are only 9 alternatives 0-8
+"""
+
+lifes = [0]*9
+for fish in data:
+    lifes[fish] += 1
+    
+print('starting point lives', lifes)
+days = 256   
 for day in range(days):
-    add_fishes = 0
-    add_fishes_idx = []
     
-    for idx, fish in enumerate(data):
-        if fish == 0:
-            add_fishes += 1
-            add_fishes_idx.append(idx)
-
-
-    # update fish count
-    data = [fish-1 for fish in data]
     
-    # reset fishes to 6 and add new fish
-    for idx in add_fishes_idx:
-        data[idx] = 6
-        data.append(8)
+    #first save nmbr of fishes with 0 lives
+    life0 = lifes[0]
+    for ii in range(8):
+        lifes[ii] = lifes[ii+1]
         
-   # print('at the end of the day',data)
         
+    lifes[8] = life0
+    # the 0-lifes should be reset to 6 lifes
+    lifes[6] += life0
+   #print('lifes after one day ', lifes)
 
-print('\n' + 'result part 1: ', len(data) )
+    
 
-
-
-   
-print('\n' + 'result part 2: ', )
+print('\n' + 'result part 2: ', sum(lifes) )
 
